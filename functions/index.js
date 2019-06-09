8b8620703fa5b14072c1f51dff56ae5346eea9b7
Johnny Ordoñez <johnny.ord.mzk@gmail.com>
2019-06-09 19:01:43 -0400

    “nsubiendo mi primera API Firebase

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+// Requerimos Firebase Admin
+const firebase = require('firebase-admin');
+// Creamos un archivo con nuestra configuración.
+const config = require('./firebase-config.json');
+
+// inicializamos nuestra aplicacion
+firebase.initializeApp({
+  credential: firebase.credential.cert(config),
+  databaseURL: 'https://curriculum-dev-johnny.firebaseio.com'
+});
+
+// creaamos la funcion que obtiene los recursos de nuestra firebase database
+exports.api = functions.https.onRequest((req, res) => {
+  res.header('Content-Type', 'application/json');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'GET') {
+    const data = firebase.database().ref('/me')
+    data.on('value', (snapshot) => {
+      res.json(snapshot.val());
+    });
+  }
+});
